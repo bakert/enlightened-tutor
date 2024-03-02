84d169fb920d6efbc3ac21fee8456f209f8280d1
Thomas David Baker <bakert@gmail.com>
2024-03-01 22:37:48 -0800

    Set up database

---
+++ enlightened-tutor.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE enlightened_tutor;
+
+USE enlightened_tutor;
+
+CREATE TABLE IF NOT EXISTS event (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    format VARCHAR(4) NOT NULL,
+    date DATETIME NOT NULL,
+    level INT NOT NULL,
+    num_players INT,
+    url VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS deck (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    rank_high INT,
+    rank_low INT,
+    player VARCHAR(255) NOT NULL,
+    event_id INT NOT NULL,
+    FOREIGN KEY (event_id) REFERENCES event(id)
+);
+
+CREATE TABLE IF NOT EXISTS deck_card (
+    deck_id INT NOT NULL,
+    FOREIGN KEY (deck_id) REFERENCES deck(id),
+    card VARCHAR(255) NOT NULL,
+    num INT NOT NULL,
+    sideboard BOOLEAN NOT NULL
+);
